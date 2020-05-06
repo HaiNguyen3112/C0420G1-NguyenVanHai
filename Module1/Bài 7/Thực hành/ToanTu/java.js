940bcf3b9b3160432252e6a87c645773cdf671b8
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-05-07 01:38:12 +0700

    Up them bai

---
+++ Module1/Bài 7/Thực hành/ToanTu/java.js
@@ -0,0 +1,6 @@
+inputWidth=prompt("Nhập chiều rộng: ");
+inputHeight=prompt("Nhập chiều cao: ");
+let area = inputWidth*inputHeight;
+document.write("Chiều cao: "+inputHeight);
+document.write("<br/>Chiều rộng: "+inputWidth);
+document.write("<br/>Diện tích= "+area);
