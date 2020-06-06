523f6487e87b306c9db090ac74d48a314d8db414
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-06-06 15:35:50 +0700

    100prime

---
+++ Module 2/src/IntroductionJava/BàiTap/Bai2/PrimeUnder100.java
@@ -0,0 +1,19 @@
+package IntroductionJava.BàiTap.Bai2;
+
+public class PrimeUnder100 {
+    public static void main(String[] args) {
+        int i,j;
+        String text ="";
+        for (i =2;i<100;i++){
+            for (j =2;j<i;j++){
+                if (i%j==0){
+                    break;
+                }
+            }
+            if (j==i){
+                text +=i+" ";
+            }
+        }
+        System.out.printf("%s",text);
+    }
+}
