6a966c9291a094eb50e4f08e0848858f192d85f8
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-06-06 22:51:18 +0700

    min

---
+++ Module 2/src/IntroductionJava/TH/Bai3/FindMmn.java
@@ -0,0 +1,19 @@
+package IntroductionJava.TH.Bai3;
+
+public class FindMmn {
+    public static int minValue(int [] arr){
+        int index = 0;
+        for (int i = 1; i< arr.length;i++){
+            if (arr[index] > arr[i])
+            {
+                index =i;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        int []arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + arr[index] + " at "+(index+1));
+    }
+}
