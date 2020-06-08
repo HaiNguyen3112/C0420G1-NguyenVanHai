cea298cc8741d543fcc6bf85804b39f6ac14bd67
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-06-08 21:08:30 +0700

    time

---
+++ Module 2/src/IntroductionJava/BàiTap/Bai4/StopWatch.java
@@ -0,0 +1,30 @@
+package IntroductionJava.BàiTap.Bai4;
+
+import java.util.Date;
+
+public class StopWatch {
+    private Date startTime;
+    private Date endTime;
+    public StopWatch(){
+        this.startTime = new Date();
+    }
+    public void setStartTime(Date time){
+        this.startTime = time;
+    }
+    public void setEndTime(Date time){
+        this.endTime = time;
+    }
+    public Date start(){
+        this.startTime = new Date();
+        return  this.startTime;
+    }
+    public Date stop(){
+        this.endTime = new Date();
+        return  this.endTime;
+    }
+    public Date getElapsedTime(){
+        return start() - stop();
+    }
+
+
+}
