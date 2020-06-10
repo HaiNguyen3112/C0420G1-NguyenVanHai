5d65a2e8ab7cab2a497ac24aac93dd9124e40b99
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-06-10 16:25:45 +0700

    Circle

---
+++ Module 2/src/IntroductionJava/BàiTap/Bai5/Circle.java
@@ -0,0 +1,21 @@
+package IntroductionJava.BàiTap.Bai5;
+
+public class Circle {
+    private double radius =1.0;
+    private String color = "red";
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle() {
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return radius*radius*3.14;
+    }
+}
---
+++ Module 2/src/IntroductionJava/BàiTap/Bai5/TestCircle.java
@@ -0,0 +1,13 @@
+package IntroductionJava.BàiTap.Bai5;
+
+import IntroductionJava.BàiTap.Bai5.Circle;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle a = new Circle(3);
+
+        System.out.println("Area: "+a.getArea());
+        System.out.println("Radius: "+a.getRadius());
+
+    }
+}
