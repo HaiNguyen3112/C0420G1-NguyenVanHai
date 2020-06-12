7fa49b4b52adec43428b37fb6c8acf2de72d7e5f
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-06-12 14:02:37 +0700

    compare

--- Module 2/src/IntroductionJava/BàiTap/Bai5/Circle.java
+++ Module 2/src/IntroductionJava/BàiTap/Bai5/Circle.java
@@ -4,7 +4,7 @@ public class Circle {
     private double radius =1.0;
     private String color = "red";
 
-    public Circle(double radius) {
+    public Circle(double radius, String idigo, boolean b) {
         this.radius = radius;
     }
 
--- Module 2/src/IntroductionJava/BàiTap/Bai5/TestCircle.java
+++ Module 2/src/IntroductionJava/BàiTap/Bai5/TestCircle.java
@@ -1,10 +1,8 @@
 package IntroductionJava.BàiTap.Bai5;
 
-import IntroductionJava.BàiTap.Bai5.Circle;
-
 public class TestCircle {
     public static void main(String[] args) {
-        Circle a = new Circle(3);
+        Circle a = new Circle(3, "idigo", false);
 
         System.out.println("Area: "+a.getArea());
         System.out.println("Radius: "+a.getRadius());
