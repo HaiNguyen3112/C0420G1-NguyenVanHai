62ddb023c3e8064a6b31188ca99546f9dd563b99
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-06-15 11:21:55 +0700

    list

---
+++ Module 2/src/IntroductionJava/TH/Bai10/List/MyListTest.java
@@ -0,0 +1,15 @@
+package IntroductionJava.TH.Bai10.List;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<Integer>();
+        listInteger.add(1);
+        listInteger.add(7);
+        listInteger.add(5);
+        listInteger.add(4);
+
+//        System.out.println("element 2: "+listInteger.get(6));
+        listInteger.get(-1);
+        System.out.println("element -1: " + listInteger.get(-1));
+    }
+}
