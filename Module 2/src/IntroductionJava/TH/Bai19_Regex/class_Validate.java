ca2a107a1cc46baa31f11c8723aef5dd453b72ce
HaiNguyen3112 <nguyenhai311294@gmail.com>
2020-06-30 10:55:48 +0700

    class name regex

---
+++ Module 2/src/IntroductionJava/TH/Bai19_Regex/class_Validate.java
@@ -0,0 +1,27 @@
+package IntroductionJava.TH.Bai19_Regex;
+
+import org.omg.CORBA.MARSHAL;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class class_Validate {
+    private static final String CLASS_REGEX = "^[C|A|P][0-9]{4}[G|H|I|K|L|M]$";
+    public class_Validate(){
+
+    }
+    public boolean validate(String regex){
+        Pattern pattern = Pattern.compile(CLASS_REGEX);
+        Matcher matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+
+    public static void main( String[] args ) {
+        class_Validate temp = new class_Validate();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the class's name:");
+        String str = sc.nextLine();
+        System.out.println(temp.validate(str));
+    }
+}
